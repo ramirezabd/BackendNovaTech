836721171bf12da4fe8c539488f9d30996550cbf
Fajar Abdurrahman <kamentosa@gmail.com>
2023-02-04 14:58:11 +0700

    remove cookie-parser module and refactor server.js code

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
-const dotenv = require("dotenv")
-dotenv.config();
-
+const bodyParser = require("body-parser")
+const multer = require("multer")
 const app = express();
+const dotenv = require("dotenv")
+const routes = require("./src/routes/data");
+
+dotenv.config();
 app.use(cors());
-app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
-const routes = require("./src/routes/data");
-app.use("/api/start",routes);
+app.use(multer().single("none"));
+
+app.use("/api/start", routes);
 
 const { TEST_PORT } = process.env;
 const port = process.env.PORT || TEST_PORT;
